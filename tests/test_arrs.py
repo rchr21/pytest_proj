49f04c7c4b5553b23122e4995884295b33685c86
rchr21 <romanishe2197@gmail.com>
2023-06-09 17:49:20 +0300

    Covered the program with tests by 100%

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -9,3 +9,11 @@ def test_get():
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3, 4, 5]) == [1, 2, 3, 4, 5]
+    assert arrs.my_slice([1, 2, 3, 4, 5], 2, -1) == [3, 4]
+    assert arrs.my_slice([1, 2, 3, 4, 5], -10, 3) == [1, 2, 3]
+    assert arrs.my_slice([], 0, 3) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5], 10, 3) == []
+    assert arrs.my_slice([1, 2, 3, 4, 5], 2, 10) == [3, 4, 5]
+    assert arrs.my_slice([1, 2, 3, 4, 5], 2) == [3, 4, 5]
+    assert arrs.my_slice([1, 2, 3, 4, 5], -3, -1) == [3, 4]
